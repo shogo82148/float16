72439f45658b270de7ca4de832236cbff025a670
ICHINOSE Shogo <shogo82148@gmail.com>
2023-10-13 09:55:22 +0900

    implement hex formatting

--- strconv.go
+++ strconv.go
@@ -1,6 +1,7 @@
 package float16
 
 import (
+	"math/bits"
 	"strconv"
 )
 
@@ -28,13 +29,116 @@ func (x Float16) Append(buf []byte, fmt byte, prec int) []byte {
 			buf = append(buf, '-')
 		}
 		buf = append(buf, '0', fmt)
-		if x&^signMask16 == 0 {
-			if fmt == 'x' {
-				return append(buf, '0', 'p', '+', '0', '0')
+
+		// normalize x
+		exp := int((x >> shift16) & mask16)
+		frac := uint16(x & fracMask16)
+		if exp == 0 && frac == 0 {
+			// zero
+			buf = append(buf, '0')
+			if prec >= 1 {
+				buf = append(buf, '.')
+				for i := 0; i < prec; i++ {
+					buf = append(buf, '0')
+				}
+			}
+			buf = append(buf, fmt-('x'-'p'))
+			buf = append(buf, '+', '0', '0')
+			return buf
+		}
+		if exp == 0 {
+			// subnormal number
+			l := bits.Len32(uint32(frac))
+			frac <<= shift16 - l + 1
+			exp = -(bias16 + shift16) + l
+		} else {
+			// normal number
+			frac |= 1 << shift16
+			exp -= bias16
+		}
+
+		switch prec {
+		case -1:
+			if frac&0x3ff == 0 {
+				buf = append(buf, '1')
+			} else if frac&0x3f == 0 {
+				buf = append(buf, '1', '.')
+				buf = append(buf, nibble(fmt, frac>>6))
+			} else if frac&0x3 == 0 {
+				buf = append(buf, '1', '.')
+				buf = append(buf, nibble(fmt, frac>>6))
+				buf = append(buf, nibble(fmt, frac>>2))
 			} else {
-				return append(buf, '0', 'P', '+', '0', '0')
+				buf = append(buf, '1', '.')
+				buf = append(buf, nibble(fmt, frac>>6))
+				buf = append(buf, nibble(fmt, frac>>2))
+				buf = append(buf, nibble(fmt, frac<<2))
+			}
+
+		case 0:
+			// round to nearest even
+			frac += 1 << (shift16 - 1)
+			if frac >= 1<<(shift16+1) {
+				exp++
+				frac >>= 1
+			}
+
+			buf = append(buf, '1')
+
+		case 1:
+			// round to nearest even
+			frac += 0x1f + (frac>>6)&1
+			if frac >= 1<<(shift16+1) {
+				exp++
+				frac >>= 1
+			}
+
+			buf = append(buf, '1', '.')
+			buf = append(buf, nibble(fmt, frac>>6))
+
+		case 2:
+			// round to nearest even
+			frac += 1 + (frac>>2)&1
+			if frac >= 1<<(shift16+1) {
+				exp++
+				frac >>= 1
+
+			}
+
+			buf = append(buf, '1', '.')
+			buf = append(buf, nibble(fmt, frac>>6))
+			buf = append(buf, nibble(fmt, frac>>2))
+
+		default:
+			if prec < 0 {
+				panic("invalid precision")
+			}
+			buf = append(buf, '1', '.')
+			buf = append(buf, nibble(fmt, frac>>6))
+			buf = append(buf, nibble(fmt, frac>>2))
+			buf = append(buf, nibble(fmt, frac<<2))
+			for i := 3; i < prec; i++ {
+				buf = append(buf, '0')
 			}
 		}
+
+		buf = append(buf, fmt-('x'-'p'))
+		if exp >= 0 {
+			buf = append(buf, '+')
+		} else {
+			buf = append(buf, '-')
+			exp = -exp
+		}
+		buf = append(buf, byte(exp/10)+'0', byte(exp%10)+'0')
+		return buf
 	}
 	return strconv.AppendFloat(buf, x.Float64(), fmt, prec, 32)
+}
+
+func nibble(fmt byte, x uint16) byte {
+	x &= 0xf
+	if x < 10 {
+		return '0' + byte(x)
+	}
+	return ('a' + byte(x-10)) | (fmt & 0x20)
 }
--- strconv_test.go
+++ strconv_test.go
@@ -1,6 +1,9 @@
 package float16
 
-import "testing"
+import (
+	"strconv"
+	"testing"
+)
 
 func TestString(t *testing.T) {
 	tests := []struct {
@@ -68,7 +71,6 @@ func TestFormat(t *testing.T) {
 		{NaN(), 'b', -1, "NaN"},
 
 		// decimal exponent formats
-
 		{0, 'e', -1, "0e+00"},
 		{0x8000, 'e', -1, "-0e+00"},
 		{Inf(1), 'e', -1, "+Inf"},
@@ -95,12 +97,50 @@ func TestFormat(t *testing.T) {
 		{Inf(-1), 'g', -1, "-Inf"},
 		{NaN(), 'g', -1, "NaN"},
 
+		{0, 'G', -1, "0"},
+		{0x8000, 'G', -1, "-0"},
+		{Inf(1), 'G', -1, "+Inf"},
+		{Inf(-1), 'G', -1, "-Inf"},
+		{NaN(), 'G', -1, "NaN"},
+
 		// hexadecimal formats
 		{0, 'x', -1, "0x0p+00"},
 		{0x8000, 'x', -1, "-0x0p+00"},
 		{Inf(1), 'x', -1, "+Inf"},
 		{Inf(-1), 'x', -1, "-Inf"},
 		{NaN(), 'x', -1, "NaN"},
+
+		{FromFloat64(0), 'x', 0, "0x0p+00"},
+		{FromFloat64(0x1.0p0), 'x', 0, "0x1p+00"},
+		{FromFloat64(0x1.7p0), 'x', 0, "0x1p+00"},
+		{FromFloat64(0x1.8p0), 'x', 0, "0x1p+01"},
+
+		{FromFloat64(0x0.0p0), 'x', 1, "0x0.0p+00"},
+		{FromFloat64(0x1.0p0), 'x', 1, "0x1.0p+00"},
+		{FromFloat64(0x1.fp0), 'x', 1, "0x1.fp+00"},
+		{FromFloat64(0x1.ffc0p0), 'x', 1, "0x1.0p+01"},
+		{FromFloat64(0x1.08p0), 'x', 1, "0x1.0p+00"},
+		{FromFloat64(0x1.18p0), 'x', 1, "0x1.2p+00"},
+
+		{FromFloat64(0x0.00p0), 'x', 2, "0x0.00p+00"},
+		{FromFloat64(0x1.00p0), 'x', 2, "0x1.00p+00"},
+		{FromFloat64(0x1.0fp0), 'x', 2, "0x1.0fp+00"},
+		{FromFloat64(0x1.ffc0p0), 'x', 2, "0x1.00p+01"},
+		{FromFloat64(0x1.008p0), 'x', 2, "0x1.00p+00"},
+		{FromFloat64(0x1.018p0), 'x', 2, "0x1.02p+00"},
+
+		{FromFloat64(0x0.000p0), 'x', 3, "0x0.000p+00"},
+		{FromFloat64(0x1.000p0), 'x', 3, "0x1.000p+00"},
+		{FromFloat64(0x1.00c0p0), 'x', 3, "0x1.00cp+00"},
+		{FromFloat64(0x1.ffc0p0), 'x', 3, "0x1.ffcp+00"},
+
+		{FromFloat64(0x1.ffc0p0), 'x', 4, "0x1.ffc0p+00"},
+		{FromFloat64(0x1p-24), 'x', 4, "0x1.0000p-24"},
+
+		{FromFloat64(0x1p0), 'x', -1, "0x1p+00"},
+		{FromFloat64(0x1.8p0), 'x', -1, "0x1.8p+00"},
+		{FromFloat64(0x1.08p0), 'x', -1, "0x1.08p+00"},
+		{FromFloat64(0x1.00cp0), 'x', -1, "0x1.00cp+00"},
 
 		{0, 'X', -1, "0X0P+00"},
 		{0x8000, 'X', -1, "-0X0P+00"},
@@ -115,4 +155,35 @@ func TestFormat(t *testing.T) {
 			t.Errorf("%#v: expected %s, got %s", tt, tt.s, got)
 		}
 	}
+}
+
+func FuzzFormat(f *testing.F) {
+	f.Add(uint16(0), 'b', 1)
+	f.Add(uint16(0), 'e', 1)
+	f.Add(uint16(0), 'E', 1)
+	f.Add(uint16(0), 'f', 1)
+	f.Add(uint16(0), 'g', 1)
+	f.Add(uint16(0), 'G', 1)
+	f.Add(uint16(0), 'x', 1)
+	f.Add(uint16(0), 'X', 1)
+	f.Fuzz(func(t *testing.T, x uint16, fmt rune, prec int) {
+		if prec < 0 || prec > 100 {
+			return
+		}
+		switch fmt {
+		case 'b', 'e', 'E', 'f', 'g', 'G', 'x', 'X':
+		default:
+			return
+		}
+		if x&signMask16 == 0 && fmt == 'b' {
+			return
+		}
+
+		f := FromBits(x)
+		got := f.Format(byte(fmt), prec)
+		want := strconv.FormatFloat(f.Float64(), byte(fmt), prec, 64)
+		if got != want {
+			t.Errorf("expected %s, got %s", want, got)
+		}
+	})
 }
